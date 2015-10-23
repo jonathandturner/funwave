b7aff309e0d3ecfb78053c02a547751f1774b660
Jonathan Turner <probata@hotmail.com>
2015-10-23 14:04:01 -0700

    Read more of the fmt chunk

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
+mod wave_format {
+    pub const PCM        : u16 = 0x0001;
+    pub const IEEE_FLOAT : u16 = 0x0003;
+    pub const A_LAW      : u16 = 0x0006;
+    pub const MU_LAW     : u16 = 0x0007;
+    pub const EXTENSIBLE : u16 = 0xFFFE;
+}
+
+#[derive(Debug)]
+struct FmtChunk {
+    tag: u16,
+    num_chans: u16,
+    samples_per_sec: u32,
+    avg_bytes_per_sec: u32,
+    block_align: u16,
+    bits_per_sample: u16,
+}
+
 fn load_4byte_id(file: &mut File, id:&[u8; 4]) -> Result<(), io::Error> {
     use std::io::{Error, ErrorKind};
 
@@ -24,20 +42,55 @@ fn load_u32_le(file: &mut File) -> Result<u32, io::Error> {
         ((buffer[2] as u32) << 16) + ((buffer[3] as u32) << 24))    
 }
 
+fn load_u16_le(file: &mut File) -> Result<u16, io::Error> {
+    let mut buffer = [0; 2];
+
+    try!(file.read(&mut buffer));
+    
+    Ok(buffer[0] as u16 + ((buffer[1] as u16) << 8))    
+}
+
+fn load_wave_format(file: &mut File) -> Result<u16, io::Error> {
+    use std::io::{Error, ErrorKind};
+
+    let fmt_tag = try!(load_u16_le(file)); 
+
+    match fmt_tag {
+        wave_format::PCM        | 
+        wave_format::IEEE_FLOAT | 
+        wave_format::A_LAW      | 
+        wave_format::MU_LAW     | 
+        wave_format::EXTENSIBLE => Ok(fmt_tag),
+        _  => Err(Error::new(ErrorKind::InvalidInput, "File not in WAVE format"))
+    }    
+}
+
 fn load_wave(filename: &String) -> Result<(), io::Error> {
     let mut f = try!(File::open(filename));
 
+    //Read the RIFF header
     try!(load_4byte_id(&mut f, b"RIFF"));
-    
     let riff_size = try!(load_u32_le(&mut f)) - 4;
-    println!("Riff size: {}", riff_size);
-
     try!(load_4byte_id(&mut f, b"WAVE"));
-    
-    //Next, the WAVES chunk
+                
+    //Next, the WAVE chunk
     //which starts with a fmt chunk
     try!(load_4byte_id(&mut f, b"fmt "));
-     
+    let fmt_chunk_size = try!(load_u32_le(&mut f));
+    
+    let result = FmtChunk {
+        tag: try!(load_wave_format(&mut f)),
+        num_chans: try!(load_u16_le(&mut f)),
+        samples_per_sec: try!(load_u32_le(&mut f)),
+        avg_bytes_per_sec: try!(load_u32_le(&mut f)),
+        block_align: try!(load_u16_le(&mut f)),
+        bits_per_sample: try!(load_u16_le(&mut f))
+    };
+    
+    println!("Riff size: {}", riff_size);
+    println!("fmt size: {}", fmt_chunk_size);
+    println!("fmt: {:?}", result);
+
     Ok(())
 }
 
